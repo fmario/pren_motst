0860859362a16bc2ff44721aa7d42c87c2c6dd6e
fmario <mariofelder@bluewin.ch>
2014-04-04 15:14:35 +0200

    Structs für Protokoll UART erstellt

--- source/protocol.c
+++ source/protocol.c
@@ -12,4 +12,30 @@
 *	Compiler:		HI-TECH C Compiler for PIC18 (v9.8)
 **/
 
+#include "platform/definitions.h"
+
 #include "protocol.h"
+
+/**
+*	Function:	swap16
+*	Parameter:	(uint16) val
+*	Return:		uint16
+*
+*	Description:
+*	Ändert den Endian-Typ einer 16bit Zahl
+**/
+uint16 swap16(uint16 val){
+	return (val << 8) | (val >> 8 );
+}
+
+/**
+*	Function:	swap24
+*	Parameter:	(uint24) val
+*	Return:		uint24
+*
+*	Description:
+*	Ändert den Endian-Typ einer 24bit Zahl
+**/
+uint24 swap24(uint24 val){
+	return (val << 16) | (val >> 16) | (val & 0x0F0);
+}
--- source/protocol.h
+++ source/protocol.h
@@ -13,10 +13,116 @@
 **/
 
 /**------------------ PROTOTYPES ------------------**/
-int main(void);
+uint16 swap16(uint16 val);
+uint24 swap24(uint24 val);
 
 /**------------------ DEFINES ------------------**/
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
+
+/**--------------------------------
+* --------- UART Protocol ---------
+* -------------------------------*/
+struct InitMovePayload
+{
+    uint8 motor;
+    uint8 direction;
+};
+
+struct MoveToPayload
+{
+    uint8 motor;
+    uint8 direction;
+    uint24 absPos;
+    uint8 speed;
+    uint8 acc;
+    uint8 dec;
+};
+
+struct WaitMovedPayload
+{
+    uint8 motor;
+    uint16 timeout;
+};
+
+struct IsReadyPayload
+{
+    uint8 motor;
+};
+
+struct IsReadyResultPayload
+{
+    uint8 isReady;
+};
+
+struct MovePayload
+{
+    uint8 motor;
+    uint8 direction;
+    uint8 speed;
+    uint8 acc;
+    uint8 dec;
+};
+
+struct StopMovePayload
+{
+    uint8 motor;
+    uint8 isHardStop;
+};
+
+struct GetAbsPosPayload
+{
+    uint8 motor;
+};
+
+struct GetAbsPosResultPayload
+{
+    uint24 position;
+};
+
+struct SetPinPayload
+{
+    uint8 motor;
+    uint8 isHigh;
+};
+
+struct GetPinPayload
+{
+    uint8 motor;
+};
+
+struct GetPinResultPayload
+{
+    uint8 isHigh;
+};
+
+struct ConfigPinPayload
+{
+    uint8 motor;
+    uint8 isOutput;
+};
+
+struct SaveHomePayload{
+	uint8 motor;
+};
+
+struct GoHomePayload{
+	uint8 motor;
+};
+
+struct SaveWayPointPayload{
+	uint8 motor;
+};
+
+struct MoveToWayPoint{
+	uint8 motor;
+	uint8 wayPoint;
+	uint8 acc;
+	uint8 dcc;
+};
+
+/**--------------------------------
+* --------- SPI Protocol ----------
+* -------------------------------*/
 
 #endif  // !PROTOCOL_H
