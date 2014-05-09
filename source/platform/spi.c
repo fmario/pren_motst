0a7eaf191685b7a47ee5ce53d22e86835a60a924
fmario <mariofelder@bluewin.ch>
2014-05-09 11:30:36 +0200

    SPI implementiert

--- source/platform/spi.c
+++ source/platform/spi.c
@@ -26,24 +26,25 @@
 *	Initialisiert die SPI-Schnittstelle
 **/
 void init_spi(void){
-	TRISC5 = 0;			// Clear for SDO
 	TRISC3 = 0;			// Clear for SCK
+	TRISC4 = 1;			// Set for SDI
+	TRISC5 = 0;			// Clear for SDO
 
 	SMP = 0;			// Input sampled at middle of data output time
 	CKE = 0;			// Transmit occurs on idle to active clock state
 
 	CKP = 1;			// Idle state for clock is high level
-	SSPM0 = 0;			// SPI Master Mode, clock = Fosc/4
+	SSPM0 = 1;			// SPI Master Mode, clock = Fosc/8
 	SSPM1 = 0;
 	SSPM2 = 0;
 	SSPM3 = 0;
 	
-	SSPIE = 1;			// SPI Interrupt enabled
+	SSPIE = 0;			// SPI Interrupt disabled
 	SSPEN = 1;			// Enables SPI
 }
 
 /**
-*	Function:	send_Byte
+*	Function:	spi_send_Byte
 *	Parameter:	(uint8) cByte
 *	Return:		void
 *
@@ -54,13 +55,12 @@ void init_spi(void){
 void spi_send_Byte(uint8 cByte){
 	do{
 		WCOL = 0;
-		while(BF);
 		SSPBUF = cByte;
-	}while(WCOL);
+	}while(WCOL);	//repeat if write collision
 }
 
 /**
-*	Function:	receive_Byte
+*	Function:	spi_receive_Byte
 *	Parameter:	void
 *	Return:		uint8
 *
@@ -69,38 +69,64 @@ void spi_send_Byte(uint8 cByte){
 *	sobald dieses vollständig Empfangen ist.
 **/
 uint8 spi_receive_Byte(void){
-	while(!SSPIF);
+	while(!BF);
 	return SSPBUF;
 }
 
 /**
-*	Function:	send_Array
-*	Parameter:	(uint8) iLength
-				(uint8*) cArray
+*	Function:	spi_send_Command
+*	Parameter:	(uint8) iAddress
+				(uint8) iCmdLength
+				(uint8*) cCmdArray
+				(uint8) iRspLength
+				(uint8*) cRspBuffer
 *	Return:		void
 *
 *	Description:
-*	Sendet mehrere Bytes über die UART Schnittstelle. 
-*	iLength ist die Anzahl Bytes, welche gesendet werden sollen
-*	und cArray ist der Pointer auf das erste Byte.
+*	Sendet einen Befehl über SPI an den gewählten (iAddress)
+*	Device. Falls der Befehl eine Antwort zu Folge hat, wird
+*	diese in cRspBuffer gespeichert.
 **/
-void spi_send_Array(uint8 iLength, uint8* cArray){
-	for(int i = 0; i < iLength; i++)
-		send_Byte(*(cArray + i));
+void spi_send_Command(uint8 iAddress, uint8 iCmdLength, uint8* cCmdArray, uint8 iRspLength, uint8* cRspBuffer){
+
+	for(int i = 0; i < iCmdLength; i++){
+		set_CS(iAddress);
+		spi_send_Byte(*(cCmdArray + i));
+		spi_receive_Byte();
+		reset_CS();
+	}
+
+	for(int i = 0; i < iRspLength; i++){
+		set_CS(iAddress);
+		spi_send_Byte(SPI_NOP);
+		*(cRspBuffer + i) = spi_receive_Byte();
+	}
 }
 
 /**
-*	Function:	receive_Array
-*	Parameter:	(uint8) iLength
-				(uint8*) cBuffer
+*	Function:	set_CS
+*	Parameter:	(uint8) iAddress
 *	Return:		void
 *
 *	Description:
-*	Empfängt mehrere Bytes über die UART Schnittstelle. 
-*	iLength ist die Anzahl Bytes, welche empfangen werden sollen
-*	und cBuffer ist der Pointer auf das erste Byte des reservierten Buffers.
+*	Setzt die ChipSelect-Leitung des Entprechenden
+*	Devices.
 **/
-void spi_receive_Array(uint8 iLength, uint8* cBuffer){
-	for(int i = 0; i < iLength; i++)
-		*(cBuffer + i) = receive_Byte();
+void set_CS(uint8 iAddress){
+	PORTE_IO.nCS = 1 << iAddress;
+	_delay(0x02);	//wait 400ns (t_setCS > 350ns)
+}
+
+/**
+*	Function:	reset_CS
+*	Parameter:	(void)
+*	Return:		void
+*
+*	Description:
+*	Deselecktiert alles Devices und wartet die minimale
+*	entsprechende Zeit.
+**/
+void reset_CS(void){
+	PORTE_IO.nCS = 0;
+	_delay(0x05);	//wait 1us (t_disCS > 800ns)
 }
--- source/platform/spi.h
+++ source/platform/spi.h
@@ -16,11 +16,15 @@
 void init_spi(void);
 void spi_send_Byte(uint8 cByte);
 uint8 spi_receive_Byte(void);
-void spi_send_Array(uint8 iLength, uint8* cArray);
-void spi_receive_Array(uint8 iLength, uint8* cBuffer);
+void spi_send_Command(uint8 iAddress, uint8 iCmdLength, uint8* cCmdArray, uint8 iRspLength, uint8* cRspBuffer);
+void set_CS(uint8 iAddress);
+void reset_CS(void);
 
 /**------------------ DEFINES ------------------**/
 #ifndef SPI_H
 #define SPI_H
+
+#define NUMBER_DEVICES 3
+#define SPI_NOP 0x00
 
 #endif  // !SPI_H
